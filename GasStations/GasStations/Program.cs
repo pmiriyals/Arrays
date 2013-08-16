96aeaf079d1d5369a829cd08a863d1f228a6cf64
Praneeth Miriyala <pmiriyal@gmail.com>
2013-08-16 08:44:45 -0400

    new array problems

---
+++ GasStations/GasStations/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GasStations
+{
+    class Program
+    {
+        class station
+        {
+            public int gas { get; set; }
+            public int dist { get; set; }
+            public station(int gas, int dist)
+            {
+                this.gas = gas;
+                this.dist = dist;
+            }
+        }
+
+        static void FindStartOfTour(station[] stations)
+        {
+            if (stations == null || stations.Length < 2)
+                return;
+
+            int cur_gas = stations[0].gas - stations[0].dist;   //add first gas station to the tour
+            int start = 0, end = 1;
+
+            while (start != end || cur_gas < 0)
+            {
+                while (cur_gas < 0 && start != end) //if gas is < 0 and tour is still on, then remove this station from the tour
+                {
+                    cur_gas -= stations[start].gas - stations[start].dist;
+                    start = (start + 1) % stations.Length;  //increment gas (circular array)
+
+                    if (start == 0)
+                        return; //if we came back to 1st station, then a tour cannot be formed
+                }
+
+                cur_gas += stations[end].gas - stations[end].dist;
+                end = (end + 1) % stations.Length;
+            }
+
+            Console.WriteLine("Start = {0}\nEnd = {1}", start, end);
+        }
+        
+        static void Main(string[] args)
+        {
+            int[,] mat = { 
+                         {6, 4},
+                         {0, 6},
+                         {7, 3}
+                         };
+            station[] stations = new station[mat.GetLength(0)];
+
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                stations[i] = new station(mat[i, 0], mat[i, 1]);
+            }
+
+            FindStartOfTour(stations);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MaxSumSubArr/MaxSumSubArr/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MaxSumSubMatrix
+{
+    class Program
+    {
+        static int Kadane(int[] arr, out int start, out int end)
+        {
+            start = -1;
+            end = -1;
+
+            if (arr == null || arr.Length <= 0)
+            {                
+                return -1;
+            }
+            int strt = 0;
+
+            int maxSum = 0;
+            int maxSoFar = 0;
+            int min = Int32.MinValue;
+            int mini = 0;
+            bool isallNeg = true;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                maxSoFar += arr[i];
+
+                if (maxSoFar < 0)
+                {
+                    maxSoFar = 0;
+                    strt = i + 1;
+                    if (isallNeg && min < arr[i])
+                    {
+                        min = arr[i];
+                        mini = i;
+                    }
+                }
+                else if (maxSoFar > maxSum)
+                {
+                    isallNeg = false;
+                    maxSum = maxSoFar;
+                    start = strt;
+                    end = i;
+                }
+            }
+
+            if (isallNeg)
+            {
+                start = mini;
+                end = mini;
+                maxSum = min;
+            }
+
+            return maxSum;
+        }
+
+        static void MaxSumSubMat(int[,] mat)
+        {
+            int fl = 0, fr = 0, ft = 0, fb = 0, start, end;
+            int[] temp;
+            int maxSum = Int32.MinValue;
+
+            for (int left = 0; left < mat.GetLength(1); left++)
+            {
+                temp = new int[mat.GetLength(0)];
+
+                for (int right = left; right < mat.GetLength(1); right++)
+                {
+                    for (int i = 0; i < mat.GetLength(0); i++)
+                        temp[i] += mat[i, right];
+
+                    int curMax = Kadane(temp, out start, out end);
+
+                    if (curMax > maxSum)
+                    {
+                        maxSum = curMax;
+                        fl = left;
+                        fr = right;
+                        ft = start;
+                        fb = end;
+                    }
+                }
+            }
+            Console.WriteLine("Max sum = {0}\n", maxSum);
+            printMaxSubMatrix(mat, fl, fr, ft, fb);
+        }
+
+        static void printMaxSubMatrix(int[,] mat, int fl, int fr, int ft, int fb)
+        {
+            Console.WriteLine("Max Sum Sub Matrix: ");
+            for (int i = ft; i <= fb; i++)
+            {
+                for (int j = fl; j <= fr; j++)
+                {
+                    Console.Write("{0} ", mat[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[,] mat = { 
+                         {1, 2, -1, -4, -20},
+                         {-8, -3, 4, 2, 1},
+                         {3, 8, 10, 1, 3},
+                         {-4, -1, 1, 7, -6}
+                         };
+
+            MaxSumSubMat(mat);
+            Console.ReadLine();
+        }
+    }
+}
